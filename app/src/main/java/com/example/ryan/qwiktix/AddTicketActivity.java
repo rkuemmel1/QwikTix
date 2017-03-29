742b4991b6c8899db96864bfc5115f49dad00bfa
brad-evans <brad-evans>
2017-03-28 23:07:19 -0500

    got pretty far

---
+++ app/src/main/java/com/example/ryan/qwiktix/AddTicketActivity.java
@@ -0,0 +1,13 @@
+package com.example.ryan.qwiktix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_ticket);
+    }
+}
