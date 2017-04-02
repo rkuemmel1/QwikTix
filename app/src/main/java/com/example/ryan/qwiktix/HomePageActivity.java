4b9279114fe1578c5a44988d6f5ef892e60c7a75
pryr <richard-pryor-1@uiowa.edu>
2017-04-02 12:02:10 -0500

    FirebaseListviewImplementation
    A general page is almost set up. Just needs the com.firebase.ui.* to
    work. will be getting it fixed.

--- app/src/main/java/com/example/ryan/qwiktix/HomePageActivity.java
+++ app/src/main/java/com/example/ryan/qwiktix/HomePageActivity.java
@@ -1,8 +1,39 @@
 package com.example.ryan.qwiktix;
 
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.core.view.View;
+import com.firebase.ui.FirebaseListAdapter;
+/*
+    Modified from https://firebaseui.com/docs/android/index.html in relation with the
+    FirebaseListAdapter
+
+
+ */
+
 public class HomePageActivity extends BaseActivity {
 
+    Firebase mRef;
+    FirebaseListAdapter<Ticket> mAdapter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home_page);
+        mRef = new Firebase("http://qwiktix.firebaseio.com");
+        mAdapter = new FirebaseListAdapter<Ticket>(this,Ticket.class,android.R.layout.two_line_list_item,mRef)
+        {
+            protected void populateView(View view, Ticket ticket){
+                ((TextView)view.findViewByID(android.R.id.text1)).setText(ticket.getEvent());
+                ((TextView)view.findViewByID(android.R.id.text2)).setText(ticket.getEndTime());
+            }
+        };
+        ListView
+    }
 
     int getContentViewId()
     {
