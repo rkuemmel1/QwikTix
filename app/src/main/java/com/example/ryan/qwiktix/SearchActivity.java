bef72921a04797492263cfd9449b729c4fa64340
Sydney Garcia <sydney-garcia@uiowa.edu>
2017-04-26 11:19:04 -0500

    ugh

--- app/src/main/java/com/example/ryan/qwiktix/SearchActivity.java
+++ app/src/main/java/com/example/ryan/qwiktix/SearchActivity.java
@@ -3,15 +3,10 @@ package com.example.ryan.qwiktix;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.firebase.client.FirebaseError;
-import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DatabaseError;
@@ -30,23 +25,19 @@ public class SearchActivity extends BaseActivity {
 
     private DatabaseReference mDatabase;
 
-    private ArrayList<String> strings;
+    ArrayList<String> strings = new ArrayList<String>();
 
     ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Search = (EditText) findViewById(R.id.Search);
         searchList = (ListView) findViewById(R.id.searchList);
-
-
-        ListView listView = (ListView) findViewById(R.id.searchList);
-        strings = new ArrayList<String>();
-        ArrayList<String> tickets = new ArrayList<String>();
 
         mDatabase.child("tickets").addValueEventListener(new ValueEventListener() {
             @Override
@@ -57,7 +48,7 @@ public class SearchActivity extends BaseActivity {
                 }
                 ArrayAdapter arrayAdapter = new ArrayAdapter(SearchActivity.this, android.R.layout.simple_list_item_1,
                         strings);
-                listView.setAdapter(arrayAdapter);
+                searchList.setAdapter(arrayAdapter);
             }
 
             @Override
@@ -72,16 +63,18 @@ public class SearchActivity extends BaseActivity {
         Search.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                SearchActivity.this.adapter.getFilter().filter(s);
             }
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                SearchActivity.this.adapter.getFilter().filter(s);
 
             }
             @Override
             public void afterTextChanged(Editable s) {
             }
         });
+
+
     }
 
     int getContentViewId()
