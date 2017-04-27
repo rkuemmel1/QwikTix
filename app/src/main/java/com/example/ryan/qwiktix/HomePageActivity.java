93329cf0eab1aef46e30f82fe71385e53867b4ae
pryr <richard-pryor-1@uiowa.edu>
2017-04-26 23:20:32 -0500

    grabbing lucas's message information

--- app/src/main/java/com/example/ryan/qwiktix/HomePageActivity.java
+++ app/src/main/java/com/example/ryan/qwiktix/HomePageActivity.java
@@ -2,9 +2,12 @@ package com.example.ryan.qwiktix;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.firebase.client.Firebase;
@@ -27,43 +30,46 @@ public class HomePageActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ListView ticketList = (ListView)findViewById(R.id.homeList);
-        Button sellingButton = (Button)findViewById(R.id.sellButton);
-        Button chattingButton = (Button)findViewById(R.id.chatButton);
+        ListView ticketList = (ListView) findViewById(R.id.homeList);
 
-        myAdapter = new FirebaseListAdapter<Ticket>(this,Ticket.class,R.layout.ticket_display,
+        myAdapter = new FirebaseListAdapter<Ticket>(this, Ticket.class, R.layout.ticket_display,
                 getTickets()) {
             @Override
             protected void populateView(android.view.View v, Ticket model, int position) {
-                TextView eventName = (TextView)v.findViewById(R.id.eventName);
-                TextView price = (TextView)v.findViewById(R.id.price);
-                TextView endDate = (TextView)v.findViewById(R.id.endDate);
+                TextView eventName = (TextView) v.findViewById(R.id.eventName);
+                TextView price = (TextView) v.findViewById(R.id.price);
+                TextView endDate = (TextView) v.findViewById(R.id.endDate);
+                Button sellingButton = (Button) findViewById(R.id.sellButton);
+                Button messageSeller = (Button) findViewById(R.id.messageSellerBtn);
                 //Set text
                 eventName.setText("EVENT: " + model.getEvent());
                 price.setText("PRICE: $" + Integer.toString(model.getPrice()));
                 endDate.setText("END DATE: " + model.getEndTime());
+                sellingButton.setTag(model);
+                messageSeller.setTag(model);
             }
         };
         ticketList.setAdapter(myAdapter);
-
-        //sell button: goes to seller's profile
-         sellingButton.setOnClickListener(new android.view.View.OnClickListener() {
-            @Override
-            public void onClick(android.view.View v) {
-                Intent intent = new Intent(HomePageActivity.this,ProfileActivity.class);
-                startActivity(intent);
-            }
-        });
-        //chat button: chats with seller
-        chattingButton.setOnClickListener(new android.view.View.OnClickListener() {
-            @Override
-            public void onClick(android.view.View v) {
-                Intent intent = new Intent(HomePageActivity.this,ChatActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
+
+        //chat button: chats with seller
+        public void messageSellerButton(android.view.View v){
+
+            RelativeLayout vwParentRow = (RelativeLayout) v.getParent();
+            int c = Color.CYAN;
+            vwParentRow.setBackgroundColor(c);
+
+            Ticket selectedTicket= (Ticket)v.getTag();
+            String sellerUid = selectedTicket.getuID();
+            String sellerEmail = selectedTicket.getUserEmail();
+
+            Intent ChatIntent = new Intent(HomePageActivity.this,ChatActivity.class);
+
+            ChatIntent.putExtra("com.example.ryan.qwiktix.MESSAGE",new String[]{sellerUid,sellerEmail} );
+            //ChatIntent.putExtra("com.example.ryan.qwiktix.SELLEREMAIL",sellerEmail);
+
+            startActivity(ChatIntent);
+        }
 
     int getContentViewId()
     {
