d4e76cb1e4e3dff30dc7ff2dae46d90760d167df
Sydney Garcia <sydney-garcia@uiowa.edu>
2017-04-30 16:09:11 -0500

    search works, but leaves textviews behind

--- app/src/main/java/com/example/ryan/qwiktix/SearchActivity.java
+++ app/src/main/java/com/example/ryan/qwiktix/SearchActivity.java
@@ -1,24 +1,27 @@
 package com.example.ryan.qwiktix;
 
-        import android.os.Bundle;
-        import android.text.Editable;
-        import android.text.TextWatcher;
-        import android.view.View;
-        import android.widget.ArrayAdapter;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.ListView;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
-        import com.firebase.ui.database.FirebaseListAdapter;
-        import com.google.firebase.database.DataSnapshot;
-        import com.google.firebase.database.Query;
-        import com.google.firebase.database.ValueEventListener;
-        import com.google.firebase.database.DatabaseError;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
 
 
 public class SearchActivity extends BaseActivity {
@@ -41,10 +44,16 @@ public class SearchActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+
+
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Search = (EditText) findViewById(R.id.Search);
         searchList = (ListView) findViewById(R.id.searchList);
+
+        searchAdapter();
+        searchList.setAdapter(myAdapter);
 
         mDatabase.child("tickets").addValueEventListener(new ValueEventListener() {
             @Override
@@ -53,9 +62,6 @@ public class SearchActivity extends BaseActivity {
                     String ticket = snapshot.child("event").getValue(String.class) + " " + snapshot.child("endTime").getValue(String.class);
                     strings.add(ticket);
                 }
-                ArrayAdapter arrayAdapter = new ArrayAdapter(SearchActivity.this, android.R.layout.simple_list_item_1,
-                        strings);
-
 
             }
 
@@ -65,32 +71,18 @@ public class SearchActivity extends BaseActivity {
             }
         });
 
-        myAdapter = new FirebaseListAdapter<Ticket>(this,Ticket.class,R.layout.ticket_display,
-                getTickets()) {
-            @Override
-            protected void populateView(android.view.View v, Ticket model, int position) {
-                TextView eventName = (TextView)v.findViewById(R.id.eventName);
-                TextView price = (TextView)v.findViewById(R.id.price);
-                TextView endDate = (TextView)v.findViewById(R.id.endDate);
-                Button messageSeller = (Button)v.findViewById(R.id.messageSellerBtn);
-                //Set text
-                eventName.setText("EVENT: " + model.getEvent());
-                price.setText("PRICE: $" + Integer.toString(model.getPrice()));
-                endDate.setText("END DATE: " + model.getEndTime());
-                messageSeller.setTag(model);
-            }
-
-
-        };
-
-
         //adapter = new ArrayAdapter<String>(SearchActivity.this, R.layout.search_results, R.id.results, strings);
-        searchList.setAdapter(myAdapter);
+
+
+        //searchList.setAdapter(myAdapter);
 
         Search.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                SearchActivity.this.adapter.getFilter().filter(s);
+
+                searchAdapter();
+                searchList.setAdapter(myAdapter);
+                //SearchActivity.this.myAdapter.getItem(1);
             }
 
             @Override
@@ -116,9 +108,59 @@ public class SearchActivity extends BaseActivity {
         return R.id.action_search;
     }
 
+    public void searchAdapter(){
+        myAdapter = new FirebaseListAdapter<Ticket>(this,Ticket.class,R.layout.ticket_display,
+                getTickets()) {
+            @Override
+            protected void populateView(android.view.View v, Ticket model, int position) {
+                TextView eventName = (TextView) v.findViewById(R.id.eventName);
+                TextView price = (TextView) v.findViewById(R.id.price);
+                TextView endDate = (TextView) v.findViewById(R.id.endDate);
+                Button messageSeller = (Button) v.findViewById(R.id.messageSellerBtn);
+                if(model.getEvent().toLowerCase().contains(Search.getText().toString().toLowerCase())) {
+                    //Set text
+                    eventName.setText("EVENT: " + model.getEvent());
+                    price.setText("PRICE: $" + Integer.toString(model.getPrice()));
+                    endDate.setText("END DATE: " + model.getEndTime());
+                    messageSeller.setTag(model);
+                }
 
+                else
+                {/*
+                    eventName.setVisibility(View.GONE);
+                    price.setVisibility(View.GONE);
+                    endDate.setVisibility(View.GONE);
+                    messageSeller.setVisibility(View.GONE);*/
+                }
+            }
+
+        };
+        //searchList.setAdapter(myAdapter);
+
+    }
+
+    public void messageSellerButton(android.view.View v){
+
+        LinearLayout vwParentRow = (LinearLayout)v.getParent();
+        int c = Color.CYAN;
+        vwParentRow.setBackgroundColor(c);
+
+        Ticket selectedTicket= (Ticket)v.getTag();
+        String sellerUid = selectedTicket.getuID();
+        String sellerEmail = selectedTicket.getUserEmail();
+
+        Intent ChatIntent = new Intent(SearchActivity.this,ChatActivity.class);
+
+        ChatIntent.putExtra("com.example.ryan.qwiktix.MESSAGE",new String[]{sellerUid,sellerEmail} );
+        //ChatIntent.putExtra("com.example.ryan.qwiktix.SELLEREMAIL",sellerEmail);
+
+        startActivity(ChatIntent);
+
+
+    }
 
 }
+
 
    /* Query query = mDatabase.child("tickets");//.equalTo(Search.getText().toString());
 myAdapter = new FirebaseListAdapter<Ticket>(SearchActivity.this, Ticket.class ,R.layout.ticket_display, query) {
