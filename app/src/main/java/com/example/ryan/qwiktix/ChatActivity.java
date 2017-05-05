7d33a22403d866573bad2d025dcdd562b3ccc72f
brad-evans <brad-evans>
2017-05-04 20:38:45 -0500

    messaging works great now

--- app/src/main/java/com/example/ryan/qwiktix/ChatActivity.java
+++ app/src/main/java/com/example/ryan/qwiktix/ChatActivity.java
@@ -3,6 +3,7 @@ package com.example.ryan.qwiktix;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.text.format.DateFormat;
 import android.view.View;
@@ -14,6 +15,8 @@ import android.widget.TextView;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 /**
@@ -32,6 +35,7 @@ public class ChatActivity extends BaseActivity {
     int switchedConvos = 0;
     int spinnerPosition = 0;
     private boolean chatAlreadyCreated;
+    private String otherUserName;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +50,7 @@ public class ChatActivity extends BaseActivity {
         if(intent.getStringArrayExtra("com.example.ryan.qwiktix.MESSAGE") != null) {
             //uid of user you just started a conversation with
             otherUserUid = intent.getStringArrayExtra("com.example.ryan.qwiktix.MESSAGE")[0];
-            String otherUserName = intent.getStringArrayExtra("com.example.ryan.qwiktix.MESSAGE")[1];
+            otherUserName = intent.getStringArrayExtra("com.example.ryan.qwiktix.MESSAGE")[1];
             String chatexists = intent.getStringArrayExtra("com.example.ryan.qwiktix.MESSAGE")[2];
             String chatexists2  = intent.getStringArrayExtra("com.example.ryan.qwiktix.MESSAGE")[3];
 
@@ -54,41 +58,90 @@ public class ChatActivity extends BaseActivity {
             if(chatexists.equals("true")||chatexists2.equals("true")){
                 chatAlreadyCreated = true;
             }
+            else{
+                chatAlreadyCreated = false;
+            }
 
-//            String newChatName = uid+ otherUserUid;
-//            String otherUser = otherUserUid;
-//
-//            if(chatAlreadyCreated){
-//                displayChatMessages();
-//            }
-//
+            String chatName1 = uid+ otherUserUid;
+            String chatName2 = otherUserUid + uid;
+
+            if(chatAlreadyCreated){
+
+                getMessages().child(chatName1).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        if(dataSnapshot.exists()){
+                            conversation = chatName1;
+                            displayChatMessages(chatName1);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                getMessages().child(chatName2).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        if(dataSnapshot.exists()){
+                            conversation = chatName2;
+                            displayChatMessages(chatName2);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+            }
+            else{
+                conversation = uid + otherUserUid;
+//                ChatConversation theirConvoToStart = new ChatConversation(chatName1,getmAuth().getCurrentUser().getUid(),email,"");
+//                ChatConversation convoToStart = new ChatConversation(chatName1,otherUserUid,otherUserName,"");
+//                getUsers().child(otherUserUid).child("convos").child(chatName1).setValue(theirConvoToStart);
+//                getConversations().child(chatName1).setValue(convoToStart);
+                displayChatMessages(conversation);
+            }
+
+
+
+
 //            else{
 //
+//                String newChatName = uid+ otherUserUid;
+//                //otherUserName = otherUsername[0];
+//                String otherUser = otherUserUid;
+//                ChatConversation theirConvoToStart = new ChatConversation(newChatName,getmAuth().getCurrentUser().getUid(),email,"");
+//                ChatConversation convoToStart = new ChatConversation(newChatName,otherUser,otherUserName,"");
+//                conversation = newChatName;
+//                getMessages().addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot snapshot) {
+//                        if(!snapshot.child(newChatName).exists() ){
+//                        //getMessages().child(newChatName).push().setValue(welcomeMessage);
+//                        getUsers().child(otherUser).child("convos").child(newChatName).setValue(theirConvoToStart);
 //
+//                        getConversations().child(newChatName).setValue(convoToStart);
+//                        }
+//                    }
+//                    @Override
+//                    public void onCancelled(DatabaseError e){
+//
+//                    }
+//                });
 //
 //            }
             //getUsers().child(otherUserUid);
-            String newChatName = uid+ otherUserUid;
-            //otherUserName = otherUsername[0];
-            String otherUser = otherUserUid;
-            ChatConversation theirConvoToStart = new ChatConversation(newChatName,getmAuth().getCurrentUser().getUid(),email,"");
-            ChatConversation convoToStart = new ChatConversation(newChatName,otherUser,otherUserName,"");
-            conversation = newChatName;
-            getMessages().addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot snapshot) {
-                    if(!snapshot.child(newChatName).exists() ){
-                        //getMessages().child(newChatName).push().setValue(welcomeMessage);
-                        getUsers().child(otherUser).child("convos").child(newChatName).setValue(theirConvoToStart);
 
-                        getConversations().child(newChatName).setValue(convoToStart);
-                    }
-                }
-                @Override
-                public void onCancelled(DatabaseError e){
-
-                }
-            });
 
 
 
@@ -97,28 +150,34 @@ public class ChatActivity extends BaseActivity {
             //displayConversations();
 
 
-            displayChatMessages();
+            //displayChatMessages();
 
 
             getIntent().removeExtra("com.example.ryan.qwiktix.MESSAGE");
         }
         // Load chat room contents
 
-        displayChatMessages();
+        //displayChatMessages();
 
 
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ChatMessage messageToSend = new ChatMessage(input.getText().toString(),mAuth.getCurrentUser().getEmail());
+                ChatMessage messageToSend = new ChatMessage(input.getText().toString(),getEmail());
                 //Read the input field and push a new instance
                 //of ChatMessage to the Firebase database
+
+                ChatConversation theirConvoToStart = new ChatConversation(conversation,getmAuth().getCurrentUser().getUid(),email,"");
+                ChatConversation convoToStart = new ChatConversation(conversation,otherUserUid,otherUserName,"");
+                getUsers().child(otherUserUid).child("convos").child(conversation).setValue(theirConvoToStart);
+                getConversations().child(conversation).setValue(convoToStart);
                 getMessages().child(conversation).push().setValue(messageToSend);
                 getConversations().child(conversation).child("lastMessageText").setValue("You:   "+input.getText().toString());
-                getUsers().child(otherUserUid).child("convos").child(conversation).child("lastMessageTest").setValue("them:   "+input.getText().toString());
+                getUsers().child(otherUserUid).child("convos").child(conversation).child("lastMessageText").setValue("them:   "+input.getText().toString());
                 // Clear the input
                 input.setText("");
+
             }
         });
 
@@ -128,13 +187,15 @@ public class ChatActivity extends BaseActivity {
 
 
 
-    public void displayChatMessages(){
+    public void displayChatMessages(String chatname){
+
+
 
         listOfMessages = (ListView)findViewById(R.id.list_of_messages);
         listOfMessages.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
 
         FirebaseListAdapter messageAdapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
-                R.layout.message, getMessages().child(conversation)) {
+                R.layout.message, getMessages().child(chatname)) {
 
             @Override
             protected void populateView(View v, ChatMessage model, int position) {
